d2ff037b04ac8d51c97f3a17748e5b61eecc2912
waybaba <15305665971@163.com>
2023-08-06 22:07:44 -0400

    slurm works

--- configs/hydra/launcher/submit_slurm.sh
+++ configs/hydra/launcher/submit_slurm.sh
@@ -1,10 +1,3 @@
-# Run
-
-## Start with bash file
-
-```bash
-# rew
-
 #!/bin/bash
 #SBATCH --cpus-per-task=6
 #SBATCH --tasks-per-node=1
@@ -13,7 +6,7 @@
 #SBATCH --job-name=${tags.0}
 #SBATCH --gres=gpu:p100:1
 #SBATCH --signal=USR1@120
-# #SBATCH --array=0-511%512
+# # SBATCH --array=0-511%512
 #SBATCH --time=00-3:00
 #SBATCH --job-name=delay_aware
 #SBATCH --output=./results/delay_aware.txt
@@ -30,17 +23,16 @@
 # #SBATCH --mem-per-cpu=
 
 # Mail settings
-#SBATCH --mail-user=${oc.env:EMAIL}
+#SBATCH --mail-user=wwang828@uwo.ca
 #SBATCH --mail-type=BEGIN,END,FAIL,ALL,REQUEUE
 
 echo "Running a simple task"
 
-python mbexp.py -logdir ./log/DATS \\
-    -env gym_pendulum \\
-    -o exp_cfg.exp_cfg.ntrain_iters 200 \\
-    -o exp_cfg.sim_cfg.delay_hor 10\\
-    -o ctrl_cfg.prop_cfg.delay_step 10\\
-    -ca opt-type CEM \\
-    -ca model-type PE \\
+python mbexp.py -logdir ./log/DATS \
+    -env gym_pendulum \
+    -o exp_cfg.exp_cfg.ntrain_iters 200 \
+    -o exp_cfg.sim_cfg.delay_hor 10\
+    -o ctrl_cfg.prop_cfg.delay_step 10\
+    -ca opt-type CEM \
+    -ca model-type PE \
     -ca prop-type E
-```
